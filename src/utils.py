2984ad0b859b84b4761a1fd921293813cbde00ab
Anuka Gunarathna <aharshula@gmail.com>
2025-06-04 11:54:51 +0200

    Changed alpha value to 0.2 and renamed return expressions for clarity.

--- src/utils.py
+++ src/utils.py
@@ -8,19 +8,21 @@ def dice_coef(y_true, y_pred, smooth=1e-6):
     y_true_f = tf.reshape(y_true, [-1])
     y_pred_f = tf.reshape(y_pred, [-1])
     intersection = tf.reduce_sum(y_true_f * y_pred_f)
-    return (2.0 * intersection + smooth) / (
+    dice_score = (2.0 * intersection + smooth) / (
         tf.reduce_sum(y_true_f) + tf.reduce_sum(y_pred_f) + smooth
     )
+    return dice_score
 
 
 def dice_loss(y_true, y_pred):
     """
     Dice loss, defined as 1 - Dice coefficient.
     """
-    return 1.0 - dice_coef(y_true, y_pred)
+    loss_value = 1.0 - dice_coef(y_true, y_pred)
+    return loss_value
 
 
-def bce_dice_loss_weighted(y_true, y_pred, alpha=0.9):
+def bce_dice_loss_weighted(y_true, y_pred, alpha=0.2):
     """
     Weighted combination of Binary Crossentropy and Dice Loss.
 
@@ -32,5 +34,6 @@ def bce_dice_loss_weighted(y_true, y_pred, alpha=0.9):
         Combined weighted loss
     """
     bce = tf.keras.losses.BinaryCrossentropy()(y_true, y_pred)
-    dsc = dice_loss(y_true, y_pred)
-    return alpha * bce + (1 - alpha) * dsc
+    dice = dice_loss(y_true, y_pred)
+    weighted_loss = alpha * bce + (1 - alpha) * dice
+    return weighted_loss
