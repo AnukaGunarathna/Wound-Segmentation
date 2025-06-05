a3f395b4b390b71217bc49e52597a0273104f321
Anuka Gunarathna <aharshula@gmail.com>
2025-06-05 23:07:30 +0200

    Added to centralize configuration variables.

---
+++ src/constants.py
@@ -0,0 +1,12 @@
+# Input image size for model
+IMG_SIZE = 512
+
+# Default file and directory names
+DEFAULT_MODEL_FILENAME = "segmentation_model.weights.h5"
+DEFAULT_MODEL_DIR = "model_weights"
+DEFAULT_MODEL_PATH = f"{DEFAULT_MODEL_DIR}/{DEFAULT_MODEL_FILENAME}"
+
+DEFAULT_OUTPUT_DIR = "outputs"
+
+# Google Drive ID of the zipped model weights
+GDRIVE_FILE_ID = "1Rldcue5dVF2XTp1kx3G5hBvG3xApwSaS"
